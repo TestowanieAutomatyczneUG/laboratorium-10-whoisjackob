ade4e5068511c4267f9714e2b6a4fa1876ac71dc
jszczepanski <jackobgfx@gmail.com>
2020-12-08 23:57:08 +0100

    zadanie2

--- note.py
+++ note.py
@@ -14,3 +14,38 @@ class Note:
 
     def get_note(self):
         return self.note
+
+
+class NotesStorage:
+    def __init__(self):
+        self.notes = []
+
+    def get_all_notes_of(self, name):
+        pass
+
+    def clear(self):
+        pass
+
+    def add(self, note):
+        pass
+
+
+class NotesService:
+    def __init__(self):
+        self.storage = NotesStorage()
+
+    def add(self, note):
+        self.storage.add(note)
+
+    def average_of(self, name):
+        notes = self.storage.get_all_notes_of(name)
+        if len(notes) != 0:
+            res = 0
+            for note in notes:
+                res += note.note
+            return res / len(notes)
+        else:
+            raise Exception
+
+    def clear(self):
+        self.storage.clear()
--- test_note.py
+++ test_note.py
@@ -1,5 +1,6 @@
 import unittest
-from note import Note
+from note import Note, NotesService, NotesStorage
+from unittest.mock import *
 
 
 class TestNote(unittest.TestCase):
@@ -21,3 +22,33 @@ class TestNote(unittest.TestCase):
         note = 3.5
         test_object = Note(name, note)
         self.assertEqual(test_object.get_note(), note)
+
+
+class TestNotesService(unittest.TestCase):
+    def setUp(self):
+        self.temp = NotesService()
+
+    @patch.object(NotesStorage, "get_all_notes_of")
+    def test_add_note(self, mock_method):
+        name = "Kuba"
+        note = 4.23
+        mock_method.return_value = [Note("Kuba", 4.23)]
+        self.temp.add(Note(name, note))
+        self.assertEqual(self.temp.average_of(name), 4.23)
+
+    @patch.object(NotesStorage, "clear")
+    def test_note_clear(self, mock_method):
+        mock_method.return_value = None
+        temp = NotesService()
+        self.assertEqual(temp.clear(), None)
+
+    # @patch.object(NotesStorage, "get_all_notes_of")
+    # def test_average_name_does_not_exist(self, mock_method):
+    #     self.temp.add(Note("Kuba", 5.0))
+    #     mock_method.return_value = []
+    #     self.assertEqual(self.temp.average_of("Wiktor"), "Name does not exist")
+
+    # @patch.object(NotesStorage, "get_all_notes_of")
+    # def test_average_name(self, mock_method):
+    #     mock_method.return_value = [Note("test1", 5)]
+    #     self.assertRaises(self.temp.average_of("test2"), ValueError)
