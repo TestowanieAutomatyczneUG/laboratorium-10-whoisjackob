d0fad18d57f2fcbf3f013b080622617b82b9f3ab
jszczepanski <jackobgfx@gmail.com>
2020-12-08 21:07:27 +0100

    zad1

--- test_note.py
+++ test_note.py
@@ -6,4 +6,18 @@ class TestNote(unittest.TestCase):
     def test_note_setting(self):
         Note('test', 2.23)
 
+    def test_note_error(self):
+        name = 'test'
+        note = 8
+        self.assertRaises(TypeError, Note, name, note)
 
+    def test_note_error2(self):
+        name = ''
+        note = 0.2
+        self.assertRaises(TypeError, Note, name, note)
+
+    def test_note_get_note(self):
+        name = '123'
+        note = 3.5
+        test_object = Note(name, note)
+        self.assertEqual(test_object.get_note(), note)
